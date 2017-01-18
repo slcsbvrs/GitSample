a7f91468fd3d8dfc5bf554fac0a2bc721b7b5adf
Otthoni <Otthoni@PAKS-II>
2017-01-18 19:31:17 +0100

    Calc

--- Sample1/Sample1/Program.cs
+++ Sample1/Sample1/Program.cs
@@ -10,16 +10,6 @@ namespace Sample1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1st hello");
-            Console.WriteLine("2nd hello");
-            Console.WriteLine("3rd hello");
-
-            Console.WriteLine("Calc33 commit");
-            Console.WriteLine("Calc33");
-            Console.WriteLine("Calc commit");
-            Console.WriteLine("Mondom Calc commit");
-            Console.WriteLine("Tobbszor ne mondjam h Calc commit");
-
             Console.WriteLine("Calc");
         }
     }
